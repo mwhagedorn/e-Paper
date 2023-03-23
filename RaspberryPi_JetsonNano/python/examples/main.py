c749387493b2a5a1207f58bcb17e3bb275617f3f
Mike Hagedorn <mike@silverchairsolutions.com>
2023-03-23 18:29:50 -0400

    added quotes, rearranged main

--- RaspberryPi_JetsonNano/python/examples/main.py
+++ RaspberryPi_JetsonNano/python/examples/main.py
@@ -2,12 +2,6 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
 import logging
 from waveshare_epd import epd4in2 as epd_driver
 import time
@@ -20,8 +14,14 @@ from functools import reduce
 import re
 import datetime
 
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 # Log to output file to see what's going on
-logging.basicConfig(level=logging.INFO, filename='/home/mhagedorn/e-Paper/RaspberryPi_JetsonNano/python/examples/quotes.log')
+logging.basicConfig(level=logging.INFO,
+                    filename='/home/mhagedorn/e-Paper/RaspberryPi_JetsonNano/python/examples/quotes.log')
 
 
 # Splits a long text to smaller lines which can fit in a line with max_width.
@@ -114,69 +114,97 @@ def resize(image, width, height):
 def get_affirmations():
     logging.info("Fetching affirmations..")
     affirmations = [
-        "I am committed to doing my Miracle Morning every day so that I can become the person I need to be to create everything I want for my life.",
-        "I don't judge or condemn others, because I simply have no way of knowing that if I had lived their life, I might be, say and do exactly the same",
-        "I don't worry about trying to impresss people.  Instead, I focus on how I can add value to their lives.",
+        "I am committed to doing my Miracle Morning every day so that I can become the person I need to be to create "
+        "everything I want for my life.",
+        "I don't judge or condemn others, because I simply have no way of knowing that if I had lived their life, "
+        "I might be, say and do exactly the same",
+        "I don't worry about trying to impress people.  Instead, I focus on how I can add value to their lives.",
         "There is no better day than today to do the things I've been putting off.",
-        "To reach levels of success I've never reached before, I must be committed at a level I've never been committed at before.",
-        "To avoid the activities I know I should be doing is to avoid the life I want to be living.  I will stop avoiding and start living.",
-        "It's often said that everything happens for a reason, however it is my responsibility to choose the most empowering reasons for each of the experiences in my life.",
+        "To reach levels of success I've never reached before, I must be committed at a level I've never been "
+        "committed at before.",
+        "To avoid the activities I know I should be doing is to avoid the life I want to be living.  I will stop "
+        "avoiding and start living.",
+        "It's often said that everything happens for a reason, however it is my responsibility to choose the most "
+        "empowering reasons for each of the experiences in my life.",
         "Happiness is a choice.  I choose to be happy.",
         "Today I am willing to give up who I've been for who I can become.",
-        "I am committed to taking myself to the next level so that I can take my success to the next level, because it only happens in that order",
-        "I was born with an innate inner freedom, which is my inherent ability to decide how I experience each moment in my life",
+        "I am committed to taking myself to the next level so that I can take my success to the next level, "
+        "because it only happens in that order",
+        "I was born with an innate inner freedom, which is my inherent ability to decide how I experience each moment "
+        "in my life",
         "I am happy, I am grateful for my life. I am whatever I choose to be",
         "I am disciplined. I am consistent.  I am whatever I choose to be.",
-        "Where I am in my life is a result of who I was in the past, but where I go depends entirely on who I choose to be, from this moment on.",
+        "Where I am in my life is a result of who I was in the past, but where I go depends entirely on who I choose "
+        "to be, from this moment on.",
         "If I want to upgrade my circle of influence, it begins with upgrading myself.",
         "I am destined to be whatever I choose to be.",
         "I make bold moves toward my dreams each day, refuse to give up, and know that nothing can stop me."
         "What I am capable of is determined by my potential, not my past."
         "Say it over and over until I believe it.  Then I will become it",
         "Every day I can be stressed out or blissed out.  The choice is mine",
-        "There is nothing to fear because I cannot fail.  I can only learn, grow and become better than I've ever been before",
-        "Yes I can.  Yes I will.  Yes I am able."
+        "There is nothing to fear because I cannot fail.  I can only learn, grow and become better than I've ever "
+        "been before",
+        "Yes I can.  Yes I will.  Yes I am able.",
+        "Today I choose faith over fear.",
+        "The way  to become fearless is to consistently remind myself and affirm that I can handle absolutely "
+        "anything that life throws my way.",
+        "I choose to make each day the best day of my life, which is determined not by what happens to me, but by how "
+        "I show up no matter what happens. ",
+        "I refuse to let the pain of my past limit the potential of my future.",
+        "I have faith in the miracles of life, because only those who do get to experience them.",
+        "I can't change the past, but I can change the future by changing what I do in the present.",
+        "When I focus on things that are out of my control, I feel out of control.\n So, I choose to focus only on "
+        "the one thing I have control over: being the best version of myself. ",
+        "Gratitude and complaining cannot coexist simultaneously, so I choose the one that best serves me.",
+        "I have the ability to be at peace, even when life is difficult or painful.",
+        "Anything another person has overcome or accomplished is evidence of what's possible for me.",
+
     ]
     return affirmations
 
 
-try:
-    now = datetime.datetime.now()
-    logging.info(f"\n{now.strftime('%Y-%m-%d %H:%M:%S')}")
-    logging.info(f"Waking up...")
-    epd = epd_driver.EPD()
-    epd.init()
+def main():
+    try:
+        now = datetime.datetime.now()
+        logging.info(f"\n{now.strftime('%Y-%m-%d %H:%M:%S')}")
+        logging.info(f"Waking up...")
+        epd = epd_driver.EPD()
+        epd.init()
 
-    logging.info(picdir)
-    result = get_affirmations()
+        logging.info(picdir)
+        result = get_affirmations()
 
-    screen_width = epd.width
-    screen_height = epd.height
-    line_spacing = 1
-    padding = 30
+        screen_width = epd.width
+        screen_height = epd.height
+        line_spacing = 1
+        padding = 30
 
-    view = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(view)
-    logging.info(f"CLS")
-    formatted_result = make_it_pretty(result, line_spacing, screen_height, screen_width, padding)
+        view = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(view)
+        logging.info(f"CLS")
+        formatted_result = make_it_pretty(result, line_spacing, screen_height, screen_width, padding)
 
-    quote = formatted_result["quote"]
-    offset_y = formatted_result["offset"]
-    font = formatted_result["font"]
+        quote = formatted_result["quote"]
+        offset_y = formatted_result["offset"]
+        font = formatted_result["font"]
 
-    logging.info("Updating...")
-    draw.text((padding, offset_y), quote, fill=0, align="left", spacing=line_spacing, font=font)
-    epd.display(epd.getbuffer(view))
+        logging.info("Updating...")
+        draw.text((padding, offset_y), quote, fill=0, align="left", spacing=line_spacing, font=font)
+        epd.display(epd.getbuffer(view))
 
-    logging.info("Standby...")
-    epd.sleep()
+        logging.info("Standby...")
+        epd.sleep()
 
-except IOError as e:
-    logging.info(e)
-    time.sleep(5)
+    except IOError as e:
+        logging.info(e)
+        time.sleep(5)
 
 
-except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd.epdconfig.module_exit()
-    exit()
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd.epdconfig.module_exit()
+        exit()
+
+
+if __name__ == "__main__":
+    main()
