4c772280037d0e1f43436b8a80253e8d8ed95391
Mike Hagedorn <mike@silverchairsolutions.com>
2024-11-20 08:06:55 -0500

    new affirmation

--- RaspberryPi_JetsonNano/python/examples/main.py
+++ RaspberryPi_JetsonNano/python/examples/main.py
@@ -179,6 +179,7 @@ def get_affirmations():
         "I am focused on success",
         "I am successful",
         "I am unstoppable",
+        "There's always a way when I am committed"
 
 
     ]
